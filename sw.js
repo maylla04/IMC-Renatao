731ae5aa0ec06a77016406a8789d133cfa6677bb
Maylla Nascimento <maylla.rie@gmail.com>
2023-08-21 08:36:59 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let cacheName = "my-first-pwa";
+let cacheName = "mari-may-pwa";
 let filesToCache = ["/", "/index.html",
                     "/css/style.css", "/js/main.js"];
 self.addEventListener("install", (e) => {
